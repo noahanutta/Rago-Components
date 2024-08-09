339738434f66c996a9f5eb8c71bc556ed0a49e97
Noah <103532278+noahanutta@users.noreply.github.com>
2024-08-09 13:30:15 -0700

    Pre-CoordinateFunction

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var splideImgs = document.querySelectorAll('.splide img');
+  var productLinks = document.querySelectorAll('.tvs-product .product-content');
   var splide = new Splide('.splide', {
     arrows: false,
     autoWidth:true,
@@ -8,4 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
     flickPower: 200,
   });
   splide.mount();
+  splide.on('click', function(slide){
+    splideImgs.forEach(
+      (s,index)=>{
+        s.classList.remove("active");
+        productLinks[index].classList.remove('active');
+      }
+    )
+    slide.slide.querySelector('img').classList.add('active');
+    productLinks[slide.index].classList.add('active');
+  })
 });
